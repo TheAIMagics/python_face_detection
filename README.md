22552e81a70d0320abb1ea1eae6750121c30204b
satyazmx <thakursatya6@gmail.com>
2022-09-02 16:41:05 +0530

    add ui for app detector app

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# python_face_detection
+
+<h1 align="center">Python Face Detection App </h1>
+
+<h5> This Application will detect object of interest (human face) in real time. Application will detect face either from user uploaded image or frames from web camera. </h5>
+
+## <img src="https://c.tenor.com/NCRHhqkXrJYAAAAi/programmers-go-internet.gif" width="25">  <b>Approach</b>
+
+- Use Opencv
+- Run web server with Flask
+
+## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="25"> <b> API</b>
+
+
+1. Landing page of application
+
+![Screenshot](snippets/snippet1.png)
+
+2. option to upload image
+
+![Screenshot](snippets/snippet5.png)
+
+3. Display uploaded image in frame
+
+![Screenshot](snippets/snippet2.png)
+
+4. Display face detected of uploaded image
+
+![Screenshot](snippets/snippet3.png)
+
+5. Display face detected using webcam
+
+![Screenshot](snippets/snippet4.png)
+
+ ## 💻 How to setup:
+
+
+
+Creating conda environment
+```
+conda create -p venv python==3.8 -y
+```
+
+activate conda environment
+```
+conda activate ./env
+```
+
+Install requirements
+```
+pip install -r requirements.txt
+```
+Run the live server using flask
+```
+python app.py
+```
+To launch flask ui
+```
+http://localhost:5000/
+```
+
+ ## <img src="https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width ="25"><b> Technologies Used</b>
+
+
+ <p align="center">
+
+ 1. Opencv
+ 2. Flask
+
+ ## 🏭 Industrial Use-cases 
+
+- Recognizing Drivers in Cars
+- Attendance System
+- Identifying accounts on social media
+ 
+ ## 👋 Conclusion
+
+  We have shown the way to implement face detection using image upload and webcam. 
---
+++ app.py
@@ -0,0 +1,79 @@
+
+import sys, os
+from werkzeug.utils import secure_filename
+from flask import Flask,send_file, abort, render_template, request,url_for,flash,redirect
+from face_detection.logger import logging
+from face_detection.exception import FaceDetectionException
+from face_detection.components.face_detect_image import FaceDetectionImage
+from face_detection.components.face_detect_webcam import FaceDetectionWebcam
+
+from flask import send_from_directory
+
+app=Flask(__name__)
+
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT,'uploads')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        sharing = FaceDetectionException(e,sys)
+        logging.info(sharing.error_message)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/image', methods=['GET', 'POST'])
+def upload_file():
+    print(request.files)
+    print(request.method)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print(file.filename)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return render_template("image.html")
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    print('download url')
+    img_path = os.path.join(UPLOAD_FOLDER, name)
+    print(img_path)
+    face_detect_obj = FaceDetectionImage(image_path=img_path)
+    face_detect_obj.display_image()
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/webcam', methods=['GET', 'POST'])
+def webcam():
+    try:
+        face_detect_web_obj = FaceDetectionWebcam()
+        face_detect_web_obj.load_camera()
+        return redirect(url_for('index'))
+        
+    except Exception as e:
+        raise FaceDetectionException(e,sys)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
